a63d18170d48816dc8877d4580ff1dc013a17474
Freedom Loisa <kitakayaloisa@gmail.com>
2022-12-12 12:44:15 +0300

    removed unused functions

--- app/static/script.js
+++ app/static/script.js
@@ -1,13 +1,3 @@
-const addHighlight = () => {
-  const aboutBtn = document.getElementById("about");
-  aboutBtn.classList.add("active");
-};
-
-const removeHighlight = () => {
-  const aboutBtn = document.getElementById("about");
-  aboutBtn.classList.remove("active");
-};
-
 const hoverState = () => {
   const pageBanner = document.getElementById("page-banner");
   pageBanner.style.transform = "scale(1.025)";
