3668818aff2a6e77d43874d2ad5effe9d7a66e1c
Freedom Loisa <kitakayaloisa@gmail.com>
2022-12-12 13:39:11 +0300

    created app models for db

---
+++ app/models/todo.py
@@ -0,0 +1,26 @@
+from app.extensions import db
+
+class User(db.Model):
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(50), unique=True)
+    email = db.Column(db.String(100), unique=True)
+    password = db.Column(db.String(254))
+    todos = db.relationship('Todo', backref='user')
+
+    def __repr__(self) -> str:
+        
+        return f'<User: "{self.username}">'
+
+class Todo(db.Model):
+    
+    id = db.Column(db.Integer, primary_key=True)
+    task = db.Column(db.String(100))
+    label = db.Column(db.String(50))
+    description = db.Column(db.Text)
+    due_date = db.Column(db.String(50))
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+    def __repr__(self) -> str:
+        
+        return f'<Todo: "{self.task}">'
