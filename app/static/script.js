96ff27be70aa75a90528b2f660335b5b9ffba5fb
Freedom Loisa <kitakayaloisa@gmail.com>
2022-12-12 12:32:34 +0300

    continuing with frontend development

--- app/static/script.js
+++ app/static/script.js
@@ -7,3 +7,13 @@ const removeHighlight = () => {
   const aboutBtn = document.getElementById("about");
   aboutBtn.classList.remove("active");
 };
+
+const hoverState = () => {
+  const pageBanner = document.getElementById("page-banner");
+  pageBanner.style.transform = "scale(1.025)";
+};
+
+const hoverStateReverse = () => {
+  const pageBanner = document.getElementById("page-banner");
+  pageBanner.style.transform = "scale(1)";
+};
