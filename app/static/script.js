ddbcfac72cb458990e7830ee20e72ea58872fa05
Freedom Loisa <kitakayaloisa@gmail.com>
2022-12-12 06:14:36 +0300

    added static files to use in app

---
+++ app/static/script.js
@@ -0,0 +1,9 @@
+const addHighlight = () => {
+  const aboutBtn = document.getElementById("about");
+  aboutBtn.classList.add("active");
+};
+
+const removeHighlight = () => {
+  const aboutBtn = document.getElementById("about");
+  aboutBtn.classList.remove("active");
+};
